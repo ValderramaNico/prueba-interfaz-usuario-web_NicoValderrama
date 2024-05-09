67bbb25f12fd42dc718ce8e2d96209b923efbde9
spl-NicoValderrama <valderrama.nicolas@outlook.com>
2024-05-08 21:45:13 -0400

    commit final

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,26 +1,35 @@
-const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+const alertPlaceholder = document.getElementById("liveAlertPlaceholder");
 const appendAlert = (message, type) => {
-  const wrapper = document.createElement('div')
+  const wrapper = document.createElement("div");
   wrapper.innerHTML = [
     `<div class="alert alert-${type} alert-dismissible" role="alert">`,
     `   <div>${message}</div>`,
     '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
-    '</div>'
-  ].join('')
+    "</div>",
+  ].join("");
 
-  alertPlaceholder.append(wrapper)
-}
+  alertPlaceholder.append(wrapper);
+};
 
-const alertTrigger = document.getElementById('liveAlertBtn')
+const alertTrigger = document.getElementById("liveAlertBtn");
 if (alertTrigger) {
-  alertTrigger.addEventListener('click', () => {
-    appendAlert('Tu mensaje se ha enviado!', 'info')
-  })
+  alertTrigger.addEventListener("click", () => {
+    appendAlert("Tu mensaje se ha enviado!", "info");
+  });
 }
 
 const tooltipTriggerList = document.querySelectorAll(
-    '[data-bs-toggle="tooltip"]'
-  );
-  const tooltipList = [...tooltipTriggerList].map(
-    (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
-  );
+  '[data-bs-toggle="tooltip"]'
+);
+const tooltipList = [...tooltipTriggerList].map(
+  (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
+);
+
+const navbar = document.querySelector(".navbar");
+const scrollInit=445;
+window.addEventListener("scroll", function () {
+    if (window.scrollY< scrollInit) {
+    let alpha=window.scrollY/scrollInit;
+    navbar.style.backgroundColor=`rgba(33, 37, 41,${alpha})`;
+} 
+});
